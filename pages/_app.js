25191329b396d103b98c91ec4d110bb401a98449
Patrick Conrad <patcon1994@gmail.com>
2022-02-25 09:44:17 -0500

    Vboms first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import {ContextProvider} from '../state/RootContext';
+import {getDefault} from '../layouts';
+import Styles from 'styleGuide';
 import '../styles/globals.css'
+import DefaultLayout from 'layouts/DefaultLayout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+const { metaData, ...rest } = pageProps;
+  
+  return (
+  <ContextProvider>
+    
+      <Styles component = {(styles)=>{
+          const getLayout = Component.getLayout ?? (()=> {
+            return getDefault(<Component {...rest} styles={styles} />, styles)
+          })
+
+          return getLayout(<Component {...rest} styles={styles} />, metaData);
+    }}/>
+  </ContextProvider>
+  )
 }
 
 export default MyApp
+
+
